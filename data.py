e0c8c85f0e70d8d23746c9ed27a5425691129339
Ollie <oquarm@student.unimelb.edu.au>
2022-02-05 02:14:45 +0400

    commit

--- data.py
+++ data.py
@@ -8,19 +8,19 @@ We'll use the Finance library to take in each candle (and it's previous closes) 
 features. Then we'll insert it into its row using pandas insert.
 """
 
-rawData = pd.read_csv("/Users/Ollie/Desktop/Ollie/Programming/Programming Projects/Trading_Bot/MachineLearningBot/ETHUSDT_15m_july1.csv") # Read in the original spreadsheet from TV as a Pandas dataframe.
+rawData = pd.read_csv("/Users/Ollie/Desktop/Ollie/Programming/Programming Projects/Trading_Bot/MachineLearningBot/CSVs/ETHUSDT_15m_july1.csv") # Read in the original spreadsheet from TV as a Pandas dataframe.
 rawData["RSI"] = 0.00 # Add a new column in the dataframe, whereby each value for each candle is initialised as 0 (this is obviously updated in the code)
-rawData.to_csv("/Users/Ollie/Desktop/Ollie/Programming/Programming Projects/Trading_Bot/MachineLearningBot/RSI.csv", index=False) # Save this modified dataframe into a new CSV file.
-withIndicators = pd.read_csv("/Users/Ollie/Desktop/Ollie/Programming/Programming Projects/Trading_Bot/MachineLearningBot/RSI.csv") # Read in this new CSV file as a fresh dataframe, called 'withIndicators'.
+rawData.to_csv("/Users/Ollie/Desktop/Ollie/Programming/Programming Projects/Trading_Bot/MachineLearningBot/CSVs/RSI.csv", index=False) # Save this modified dataframe into a new CSV file.
+withIndicators = pd.read_csv("/Users/Ollie/Desktop/Ollie/Programming/Programming Projects/Trading_Bot/MachineLearningBot/CSVs/RSI.csv") # Read in this new CSV file as a fresh dataframe, called 'withIndicators'.
 
 RSI_Period = 14
 
-candleCloses = withIndicators.iloc[:,[0,4,7]].values
+candleCloses = withIndicators.iloc[:,[0,4,7]].values # Isolate the candle's timestamp, its close value, and it's RSI value (initialised as 0)
 """
 We want to iterate through each candle, and using its close value and the 13 candles before its close values, construct the RSI value for that candle.
 """
-print(candleCloses[0])
-print(len(withIndicators))
+print(f"\n------\nSanity Check: The first row is: {candleCloses[0]}")
+print(f"The number of rows/candle is: {len(withIndicators)}\n------")
 for i in range(0, len(withIndicators)): # Iterate over every single candle (row) in the dataset.
     if i >= 13:         # If the row we're on has 13 candles above it, we'll use those close values *and* its own close value to calculate its RSI. So, this doesn't happen for the first 13 rows (not enough candles yet)
         close_values = [candleCloses[i-13][1], candleCloses[i-12][1], candleCloses[i-11][1], candleCloses[i-10][1], candleCloses[i-9][1], candleCloses[i-8][1], candleCloses[i-7][1], candleCloses[i-6][1], candleCloses[i-6][1], candleCloses[i-5][1], candleCloses[i-4][1], candleCloses[i-3][1], candleCloses[i-2][1], candleCloses[i-1][1], candleCloses[i][1]]
@@ -30,5 +30,5 @@ for i in range(0, len(withIndicators)): # Iterate over every single candle (row)
         # print(rsi[-1]) # rsi is actually an array of size 14 elements, and so we need the last value of it, because the first 13 are NaN. 
         withIndicators["RSI"][i] = rsi[-1] # Insert this calculated value into the 'fresh' dataframe.
 
-print(withIndicators.loc[0:20,:])
-withIndicators.to_csv("/Users/Ollie/Desktop/Ollie/Programming/Programming Projects/Trading_Bot/MachineLearningBot/RSI.csv", index=False) # The newly created CSV file (made on line 13) is overwritten with the inserted RSI values and saved.
+print(f"\nBelow will simply print the first 20 rows. Open up RSI.csv to see it all. The first 13 below obviously don't have an RSI:\n\n{withIndicators.loc[0:20,:]}")
+withIndicators.to_csv("/Users/Ollie/Desktop/Ollie/Programming/Programming Projects/Trading_Bot/MachineLearningBot/CSVs/RSI.csv", index=False) # The newly created CSV file (made on line 13) is overwritten with the inserted RSI values and saved.
