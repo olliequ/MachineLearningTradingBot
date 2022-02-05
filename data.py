d7f66a0860e9f73fefa628d04a89a0df5c0cd8e6
PikeyAU <66731279+PikeyAU@users.noreply.github.com>
2022-02-06 00:31:39 +1100

    Stoch, OBV and fixes

--- data.py
+++ data.py
@@ -26,6 +26,8 @@ Signal_Period = 9 # MACD signal period initialised at 9, subject to change.
 candleCloses = withIndicators.iloc[:,[0,4,7]].values # Isolate the candle's timestamp, its close value, and it's RSI value (initialised as 0
 justCloses = withIndicators['close'].to_numpy() # "Close" column from CSV converted to numpy array
 justVolume = withIndicators['Volume'].to_numpy() # "Volume" column from CSV converted to numpy array
+justHighs = withIndicators['high'].to_numpy() # "High" column from CSV converted to numpy array
+justLows = withIndicators['low'].to_numpy() # "Low" column from CSV converted to numpy array
 
 """
 We want to iterate through each candle, and using its close value and the 13 candles before its close values, construct the RSI value for that candle.
@@ -35,14 +37,9 @@ print(f"\n------\n***15M CANDLE DATA SET: Candles are from 01/07/21 to 04/02/22*
 print(f"The number of rows/candle is: {len(withIndicators)}\n------")
 
 #RSI
-for i in range(0, len(withIndicators)): # Iterate over every single candle (row) in the dataset.
-    if i >= 13:         # If the row we're on has 13 candles above it, we'll use those close values *and* its own close value to calculate its RSI. So, this doesn't happen for the first 13 rows (not enough candles yet)
-        close_values = [candleCloses[i-13][1], candleCloses[i-12][1], candleCloses[i-11][1], candleCloses[i-10][1], candleCloses[i-9][1], candleCloses[i-8][1], candleCloses[i-7][1], candleCloses[i-6][1], candleCloses[i-6][1], candleCloses[i-5][1], candleCloses[i-4][1], candleCloses[i-3][1], candleCloses[i-2][1], candleCloses[i-1][1], candleCloses[i][1]]
-        # print(f'The values used to calculate for RSI for this candle, {candleCloses[i][1]}, are: {close_values}') 
-        np_closes = np.array(close_values) # Needs to be converted to a numpy array for TALIB to do the RSI calculation.
-        rsi = talib.RSI(np_closes, RSI_Period)
-        # print(rsi[-1]) # rsi is actually an array of size 14 elements, and so we need the last value of it, because the first 13 are NaN. 
-        withIndicators["RSI"][i] = rsi[-1] # Insert this calculated value into the 'fresh' dataframe.
+
+rsi = talib.RSI(justCloses, RSI_Period) 
+withIndicators['RSI'] = rsi
 
 
 # MACD
@@ -53,8 +50,7 @@ withIndicators["MACDHIST"]= macdhist # Calculated MACD Hist values imported into
 
 # Bollinger Bands
 upperband, middleband, lowerband = talib.BBANDS(justCloses, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
-for i in range(0, len(middleband)):
-    withIndicators["BBW"][i] = ((upperband[i]-lowerband[i])/middleband[i])
+withIndicators["BBW"] = ((upperband - lowerband) / middleband)
 
 print(middleband.shape)
 
@@ -62,6 +58,12 @@ print(middleband.shape)
 
 real = talib.OBV(justCloses, justVolume)
 withIndicators["OBV"] = real
+
+# Stochastic
+
+slowk, slowd = talib.STOCH(justHighs, justLows, justCloses, fastk_period=14, slowk_period=1, slowk_matype=0, slowd_period=3, slowd_matype=0)
+withIndicators['SLOWK'] = slowk
+withIndicators['SLOWD'] = slowd
 
 
 print(f"\nBelow will simply print the first 20 candles. Open up withIndicators.csv to see them all. The first 13 candles below obviously don't have an RSI:\n\n{withIndicators.loc[0:19,:]}")
