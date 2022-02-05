cd44a3903494d689451ec6bc933db885c7f5842e
PikeyAU <66731279+PikeyAU@users.noreply.github.com>
2022-02-06 00:51:14 +1100

    Commit

--- data.py
+++ data.py
@@ -15,6 +15,8 @@ rawData["MACDSIGNAL"] = 0.00 # Add a new column in the dataframe, MACD Signal va
 rawData["MACDHIST"] = 0.00 # Add a new column in the dataframe, MACD Hist value initialised as 0.
 rawData["BBW"] = 0.00 # Bollinger Band Width -- a measure between the bands.
 rawData["OBV"] = 0.00 # Add new column in the dataframe, OBV value initialised as 0.
+rawData["SLOWK"] = 0.00
+rawData["SLOWD"] = 0.00
 rawData["Label"] = 0
 rawData.to_csv("./CSVs/withFeatures.csv", index=False) # Save this modified dataframe into a new CSV file.
 withIndicators = pd.read_csv("./CSVs/withFeatures.csv") # Read in this new CSV file as a fresh dataframe, called 'withIndicators'.
@@ -66,6 +68,8 @@ withIndicators['SLOWD'] = slowd
 for i in range(0, len(withIndicators)-1):
     if withIndicators['close'][i] < withIndicators['close'][i+1]:
         withIndicators['Label'][i] = 1
+
+
 
 print(f"\nBelow will simply print the first 20 candles. Open up withIndicators.csv to see them all. The first 13 candles below obviously don't have an RSI:\n\n{withIndicators.loc[0:19,:]}")
 withIndicators.to_csv("./CSVs/withFeatures.csv", index=False) # The newly created CSV file (made on line 13) is overwritten with the inserted RSI values and saved.
