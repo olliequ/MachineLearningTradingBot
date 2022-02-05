82911b662c33934e641742d474fe3b5e9e003ca0
Ollie <oquarm@student.unimelb.edu.au>
2022-02-05 17:30:41 +0400

    commit

--- data.py
+++ data.py
@@ -15,7 +15,7 @@ rawData["MACDSIGNAL"] = 0.00 # Add a new column in the dataframe, MACD Signal va
 rawData["MACDHIST"] = 0.00 # Add a new column in the dataframe, MACD Hist value initialised as 0.
 rawData["BBW"] = 0.00 # Bollinger Band Width -- a measure between the bands.
 rawData["OBV"] = 0.00 # Add new column in the dataframe, OBV value initialised as 0.
-rawData["Label"] = "DontBuy"
+rawData["Label"] = 0
 rawData.to_csv("./CSVs/withFeatures.csv", index=False) # Save this modified dataframe into a new CSV file.
 withIndicators = pd.read_csv("./CSVs/withFeatures.csv") # Read in this new CSV file as a fresh dataframe, called 'withIndicators'.
 
@@ -39,12 +39,9 @@ print(f"The number of rows/candle is: {len(withIndicators)}\n------")
 for i in range(0, len(withIndicators)): # Iterate over every single candle (row) in the dataset.
     if i >= 13:         # If the row we're on has 13 candles above it, we'll use those close values *and* its own close value to calculate its RSI. So, this doesn't happen for the first 13 rows (not enough candles yet)
         close_values = [candleCloses[i-13][1], candleCloses[i-12][1], candleCloses[i-11][1], candleCloses[i-10][1], candleCloses[i-9][1], candleCloses[i-8][1], candleCloses[i-7][1], candleCloses[i-6][1], candleCloses[i-6][1], candleCloses[i-5][1], candleCloses[i-4][1], candleCloses[i-3][1], candleCloses[i-2][1], candleCloses[i-1][1], candleCloses[i][1]]
-        # print(f'The values used to calculate for RSI for this candle, {candleCloses[i][1]}, are: {close_values}') 
         np_closes = np.array(close_values) # Needs to be converted to a numpy array for TALIB to do the RSI calculation.
         rsi = talib.RSI(np_closes, RSI_Period)
-        # print(rsi[-1]) # rsi is actually an array of size 14 elements, and so we need the last value of it, because the first 13 are NaN. 
         withIndicators["RSI"][i] = rsi[-1] # Insert this calculated value into the 'fresh' dataframe.
-
 
 # MACD
 macd, macdsignal, macdhist = talib.MACD(justCloses, Fast_Period, Slow_Period, Signal_Period) # MACD calculated using candle close data, Fast/Slow/Signal Periods can be changed at initialisation point.
@@ -64,7 +61,7 @@ withIndicators["OBV"] = real
 # Creating the labels
 for i in range(0, len(withIndicators)-1):
     if withIndicators['close'][i] < withIndicators['close'][i+1]:
-        withIndicators['Label'][i] = "Buy"
+        withIndicators['Label'][i] = 1
 
 print(f"\nBelow will simply print the first 20 candles. Open up withIndicators.csv to see them all. The first 13 candles below obviously don't have an RSI:\n\n{withIndicators.loc[0:19,:]}")
 withIndicators.to_csv("./CSVs/withFeatures.csv", index=False) # The newly created CSV file (made on line 13) is overwritten with the inserted RSI values and saved.
